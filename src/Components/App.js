20cc5f44b7c17dec697092cdd6dad8e669c146f2
coderner <shreyashukla680@gmail.com>
2021-10-28 13:44:23 +0530

    added delete contacts functionality

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
-// import './App.css';
+import { uuid } from "uuidv4";
+import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
@@ -7,8 +8,17 @@ import ContactList from "./ContactList";
 function App() {
   const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
+
   const addContactHandler = (contact) => {
-    setContacts([...contacts,contact]);
+    setContacts([...contacts , {id : uuid(), ...contact }]);
+  };
+
+  const removeContactHandler = (id) => {
+      const newContactList = contacts.filter((contact) => {
+          return contact.id !== id;
+      });
+
+      setContacts(newContactList);
   };
 
   useEffect(() => {
@@ -26,7 +36,7 @@ function App() {
     <div className="ui container">
         <Header/>
         <AddContact addContactHandler={addContactHandler}/>
-        <ContactList contacts={contacts}/>
+        <ContactList contacts={contacts} getContactId={removeContactHandler}/>
     </div>
   );
 }
--- src/Components/ContactCard.js
+++ src/Components/ContactCard.js
@@ -3,7 +3,7 @@ import ContactImage from "./contactimage.png";
 
 const ContactCard = (props) => {
 console.log(props);
-   const {name, email}= props.contact;
+   const {name, email, id}= props.contact;
    return(
     <div className="item">
         <img className="ui avatar image" src={ContactImage} alt="user" />
@@ -13,6 +13,7 @@ console.log(props);
          </div>
          <i className="trash alternate outline icon" 
          style={{ color:"red"}}
+         onClick={() => props.clickHandler(id)}
          ></i>
    </div>
    );
